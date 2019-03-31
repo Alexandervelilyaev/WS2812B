ad6790f4e73cb533c1984e47f56ea8a10ec184cb
alex\Александр <alexandervelilyaev@gmail.com>
2019-03-31 20:36:26 +0300

    created solution

---
+++ WS2812B/main.c
@@ -0,0 +1,18 @@
+/*
+ * WS2812B.c
+ *
+ * Created: 31.03.2019 20:17:45
+ * Author : Александр
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
